8875148fef47d1e8fb48fca2e04b9d62ab84f6ed
lenovo <lenovo@BALAJIS>
2024-03-05 15:54:29 +0530

    It is a core java for practical learning

---
+++ core_java_baiaji/src/Balaji/Samplejava.java
@@ -0,0 +1,15 @@
+package Balaji;
+import java.util.*;
+public class Samplejava {
+
+	public static void main(String[] args) {
+		Scanner S= new Scanner(System.in);
+		int a,b,c;
+		System.out.println();
+		a=S.nextInt();
+		b=78;
+		c=a+b;
+		System.out.println("Addition of a="+a+" and  b="+b+" is : "+c);
+	}
+
+}
