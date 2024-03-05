b59adb0de7d81f298f052ba2c31b9572a0e4be0c
lenovo <lenovo@BALAJIS>
2024-03-05 16:18:10 +0530

    It is a practical learning

---
+++ corejava/src/corejava/legend.java
@@ -0,0 +1,15 @@
+package corejava;
+import java.util.*;
+public class legend {
+
+	public static void main(String[] args) {
+		Scanner S= new Scanner(System.in);
+		int a,b,c;
+		System.out.println();
+		a=S.nextInt();
+		b=78;
+		c=a+b;
+		System.out.println("Addition of a="+a+" and  b="+b+" is : "+c);
+	}
+
+}
